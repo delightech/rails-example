f48f087db2c3520d0772c0e6d7c221c130394269
delightech <hisafumi.kikkawa@gmail.com>
2021-03-14 00:05:22 +0900

    initialize

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,17 @@ module GaeRails
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
