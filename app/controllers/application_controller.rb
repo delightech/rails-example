a9c490f0df8ccffb586966c6cfc46c7c649dc858
delightech <hisafumi.kikkawa@gmail.com>
2021-04-08 00:00:18 +0900

    Error処理追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,29 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  # 親クラスの例外処理を先に記述しなければ動作しない
+  # ここではActionControllerErrorの親クラスであるStandardErrorの例外処理を先に記述する
+  rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
+
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       "customer"
     end
+  end
+
+  private def rescue403(e)
+    @exception = e
+    render "errors/forbidden", status: 403
+  end
+
+  private def rescue500(e)
+    render "errors/internal_server_error", status: 500
   end
 end
