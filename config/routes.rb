aa682df1bb5fe3cb5c6a2ece897e6d0fdd0cb3ea
delightech <hisafumi.kikkawa@gmail.com>
2021-04-23 17:40:02 +0900

    add login link to admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   namespace :staff do
     root "top#index"
+    # HTTPメソッド URLパス Controller#Action 別名
     get "login" => "sessions#new", as: :login
     post "session" => "sessions#create", as: :session
     delete "session" => "sessions#destroy"
   end
   namespace :admin do
     root "top#index"
+    get "login" => "session#new", as: :login
+    post "session" => "sessions#create", as: :session
+    delete "session" => "sessions#destroy"
   end
   namespace :customer do
     root "top#index"
