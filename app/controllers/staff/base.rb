5f926fc01be0b717e59c2d2749e193a6f96bec17
delightech <hisafumi.kikkawa@gmail.com>
2021-04-25 01:21:35 +0900

    add session

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,8 +1,14 @@
 class Staff::Base < ApplicationController
-  private def current_staff_member
+  private def test
     if session[:staff_member_id]
       @current_staff_member ||= StaffMember.find_by(id: session[:staff_member_id])
     end
+  end
+  private def current_staff_member
+    puts "###################"
+    pp test
+    puts "###################"
+    test
   end
 
   helper_method :current_staff_member
