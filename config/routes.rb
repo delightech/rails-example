035056b99af4485b6bfde8aae82bd8fade41945a
delightech <hisafumi.kikkawa@gmail.com>
2021-05-04 02:16:17 +0900

    ルーティング改善

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
-  namespace :staff do
+  namespace :staff, path: "" do
     root "top#index"
     # HTTPメソッド URLパス Controller#Action 別名
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    # sessionはユーザーに一つなので単数リソースで表現可能
+    resource :session, only: [:create, :destroy]
+    # スタッフ画面（ユーザー個人利用）に新規作成、削除は不要のためresourceメソッドから除外する
+    resource :account, except: [:new, :create, :destroy]
   end
   namespace :admin do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    # sessionはユーザーに一つなので単数リソースで表現可能
+    resource :session, only: [:create, :destroy]
+    resources :staff_members
   end
   namespace :customer do
     root "top#index"
