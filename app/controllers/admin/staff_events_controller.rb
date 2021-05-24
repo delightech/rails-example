63423be08dc41761c2d088bfab49f6c3088bf49f
delightech <hisafumi.kikkawa@gmail.com>
2021-05-24 18:37:39 +0900

    リファクタリング

--- app/controllers/admin/staff_events_controller.rb
+++ app/controllers/admin/staff_events_controller.rb
@@ -3,19 +3,24 @@ class Admin::StaffEventsController < Admin::Base
     # staff_member_idが送信されてくるかどうかでネストされたリソースを表示するかどうか判断する
     if params[:staff_member_id]
       @staff_member = StaffMember.find(params[:staff_member_id])
-      # created_at（occured_at）で降順ソート
-      @events = @staff_member.events.order(occurred_at: :desc)
+      @events = @staff_member.events
     else
-      # created_at（occured_at）で降順ソート
-      @events = StaffEvent.order(occurred_at: :desc)
+      @events = StaffEvent
     end
+    # created_at（occured_at）で降順ソート
+    #@events = @events.order(occurred_at: :desc)
+
     # 関連付けられたモデルオブジェクトを一括取得する
     # 以下で、StaffMemberを一つずつID指定してクエリ発行するのではなく、in指定で一括取得できる
     # これによりSQLを無駄に繰り返し発行するのを防げる
-    @events = @events.includes(:member)
+    #@events = @events.includes(:member)
+
     # kaminari gemが提供するpageメソッドを使う
     # pageにnilを指定した場合は１になる
-    @events = @events.page(params[:page])
+    #@events = @events.page(params[:page])
     # 上記でDBアクセスが発生するのは、Viewで実際に使うタイミング
+
+    # 上記を全てまとめたのが以下
+    @events = @events.order(occurred_at: :desc).include(:member).page(params[:page])
   end
 end
