2717d3e6cbabb6cb20cb1d6651e3cad05f93d0ea
delightech <hisafumi.kikkawa@gmail.com>
2021-05-24 20:02:07 +0900

    validation追加

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -5,6 +5,15 @@ class StaffMember < ApplicationRecord
   # dependent: :destroyで、StaffMember削除実行時、StaffMemberを削除する前に関連するStaffEventを全て削除する
   has_many :events, class_name: "StaffEvent", dependent: :destroy
 
+  # \p{katakana} は任意のカタカナ一文字にマッチする
+  # \u{30fc} は長音符「-」1文字にマッチする
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+
+  # presence: trueで値が空の場合に失敗する（半角スペース、タブ文字も失敗する）
+  validates :family_name, :given_name, presence: true
+  # allow_blank: tureは値が空の場合はバリデーションスキップ
+  validates :family_name_kana, :given_name_kana, presence: true, format: { with: KATAKANA_REGEXP, allow_blank: true }
+
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
